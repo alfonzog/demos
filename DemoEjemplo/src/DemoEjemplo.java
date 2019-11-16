eda9e12cf2b62f992705b00064098afc7005bbcb
Lab2-42 <Lab2-42@Lab2-42.edificio10.local>
2019-11-15 19:33:58 -0600

    Mejora4

--- DemoEjemplo/src/DemoEjemplo.java
+++ DemoEjemplo/src/DemoEjemplo.java
@@ -14,7 +14,8 @@ public class DemoEjemplo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo 3");
+        System.out.println("JOSE ARMANDO");
     }
     
 }
